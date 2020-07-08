8944176f8891ae9684878e83b0f56e14eae51156
superyodi <songagreen@gmail.com>
2020-07-08 11:13:35 +0900

    카메라, 갤러리에서 사진가져오기 구현
    galleryAddPic() 수정필요

---
+++ app/src/main/java/com/example/doodlebot/MainActivity.kt
@@ -0,0 +1,200 @@
+package com.example.doodlebot
+
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.Gallery
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.nio.channels.GatheringByteChannel
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView : ImageView
+    private lateinit var btnCamera: Button
+    private lateinit var btnGallery: Button
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_GALLERY_TAKE = 2
+
+    lateinit var currentPhotoPath: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        btnCamera = findViewById(R.id.btnCamera)
+        btnGallery = findViewById(R.id.btnGallery)
+
+        btnCamera.setOnClickListener {
+            if(checkPermission()) {
+                dispatchTakePictureIntent()
+                galleryAddPic()
+            } else{
+                requestPermission()
+            }
+
+        }
+
+        btnGallery.setOnClickListener {
+            if(checkPermission()) {
+                openGalleryForImage()
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+//    카메라 권한 요청 -> 사용자에게 권한요청하는 메세지가 보여지고 예 || 아니오 선택
+    private fun requestPermission() {
+    ActivityCompat.requestPermissions(
+        this, arrayOf(READ_EXTERNAL_STORAGE, CAMERA),
+        REQUEST_IMAGE_CAPTURE
+    )
+}
+
+//    카메라 권한 체크 -> 사용자가 카메라 권한을 허용했는지 check
+    private fun checkPermission(): Boolean {
+        return (ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)}
+
+//    권한요청 결과
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d("TAG", "Permission: " + permissions[0] + "was " + grantResults[0] + "카메라 허가 완료")
+        }
+        else {
+            Log.d("TAG", "카메라 허가 실패")
+        }
+    }
+
+//    카메라 열기 (Android 7.0(Nougat) 이상을 가정한다.)
+    private fun dispatchTakePictureIntent() {
+        Intent (MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // 카메라 엑티비티가 나오고 사진(data)를 return한다.
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // 그 사진이 가야하는 File을 만든다.
+                val photoFile: File? =
+                    try {
+                        createImageFile()
+
+                    } catch (ex: IOException) {
+                        Log.d("TAG", "이미지파일 만들다가 ERROR")
+                        null
+                    }
+
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.doodlebot.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+//    카메라로 촬영한 이미지 -> 파일로 저장
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // 이미지파일의 이름만들기
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+//    갤러리에 사진 저장하기 (작동안함....)
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
+
+//    갤러리에서 사진 가져오기
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_GALLERY_TAKE)
+    }
+
+//    onActivityResult에서 사진 받기
+    override fun onActivityResult( requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            1 -> {
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+                    // 카메라에서 받은 데이터가 있을 경우
+                     val file = File(currentPhotoPath)
+
+                    // SDK 29이상부터 getBitmap() 사용할수없다
+                    if (Build.VERSION.SDK_INT < 28) {
+                        val bitmap = MediaStore.Images.Media
+                            .getBitmap(contentResolver, Uri.fromFile(file))
+                        imageView.setImageBitmap(bitmap)
+                    }
+                    else {
+                        val decode = ImageDecoder.createSource(this.contentResolver,
+                            Uri.fromFile(file))
+                        val bitmap = ImageDecoder.decodeBitmap(decode)
+                        imageView.setImageBitmap(bitmap)
+
+                    }
+                }
+            }
+            2 -> {
+                if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_GALLERY_TAKE) {
+                    imageView.setImageURI(data?.data)
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
